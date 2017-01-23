63ec62ccc89cade126066afc6385c95e1c92c6dc
Dalin Huang <dhuan023@gmail.com>
2017-01-22 19:18:02 -0500

    simple Restful API completed.

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,14 @@
 var express = require('express');
 var router = express.Router();
 
+// Models
+var Product = require('../models/product');
+
 
 // Routes
-router.get('/products', function(req, res){
-    res.send('api is working');
-});
+Product.methods(['get', 'put', 'post', 'delete']);
+Product.register(router, '/products');
+
 
 // Return router
 module.exports = router;
